85b97a9ac114d3d18e52b6f5fb53334bcd0ba857
ykato <66448467+ykato27@users.noreply.github.com>
2025-10-08 13:08:41 +0000

    リファクタリング

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+
+# 外部ファイルのインポート
+from data_loader import generate_dummy_data
+from components.kpi_summary import show_kpi_summary
+from components.step2_analysis import show_step2_analysis
+from components.step3_kpi_linkage import show_step3_linkage
+from components.step4_daily_trend import show_step4_daily_trend
+
+# ... (続く)
